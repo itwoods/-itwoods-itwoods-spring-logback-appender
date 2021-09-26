d5e04e8aab8c4c148396b2761b5ce9d1db2d2bf5
tbxyxs <tbxyxs@foxmail.com>
2021-09-26 17:37:18 +0800

    1、

---
+++ src/main/java/cn/itwoods/logback/ItwoodsSpringLogbackApplication.java
@@ -0,0 +1,13 @@
+package cn.itwoods.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItwoodsSpringLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItwoodsSpringLogbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/itwoods/logback/ItwoodsSpringLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itwoods.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItwoodsSpringLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
